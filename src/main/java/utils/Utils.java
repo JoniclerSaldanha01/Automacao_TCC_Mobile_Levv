28f0d128003053bc498125ea1f27643505797118
Automacao_Levv <jonicler.isaias.saldanha@gmail.com>
2022-08-29 18:21:51 -0300

    Commit versao funcional

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -36,56 +36,6 @@ public class Utils {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-    public static void logPrint(String strLog) {
-        String strLogFormatado = formatarNomeLog(strLog);
-        ExtentTest extentTest = TestRule.getExtentTest();
-        try {
-            efetuarPrintTela(strLogFormatado);
-            extentTest.log(Status.INFO, strLog,
-                    MediaEntityBuilder.createScreenCaptureFromPath(
-                                    System.getProperty("user.dir") + "\\src\\test\\resources\\" + strLogFormatado + ".png")
-                            .build());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void efetuarPrintTela(String strLog) {
-        File scrFile = ((TakesScreenshot) TestRule.getDriver()).getScreenshotAs(OutputType.FILE);
-        try {
-            FileUtils.copyFile(scrFile,
-                    new File(System.getProperty("user.dir") + "\\src\\test\\resources\\" + strLog + ".png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void logPass(String strMensagem) {
-        ExtentTest extentTest = TestRule.getExtentTest();
-        extentTest.log(Status.PASS, strMensagem);
-    }
-
-    public static void logFail(String strMensagem) {
-        ExtentTest extentTest = TestRule.getExtentTest();
-        extentTest.log(Status.FAIL, strMensagem);
-    }
-
-    public static void wait(int intSegundos) {
-        try {
-            synchronized (TestRule.getDriver()) {
-                TestRule.getDriver().wait(intSegundos * 1000);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static String formatarNomeLog(String strLog) {
-        LocalDateTime dataHoraAgora = LocalDateTime.now();
-        DateTimeFormatter formatoDataHora = DateTimeFormatter.ofPattern("dd_MM_yy_HH_mm_ss");
-        String strLogFormatado = strLog + "_" + dataHoraAgora.format(formatoDataHora);
-        return strLogFormatado;
-    }
+   
 
 }
